3360c595189a8dbfb27261d55ab0350640f1961d
Buxanka <ksu14102@yandex.ru>
2022-05-23 18:22:18 +0500

    Добавьте файлы проекта.

---
+++ functions.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "struct.h"
+int Randomizer(int& n, int x, int y);
+void Characteristics(characteristics& A);
+void Bulldozer(int n, characteristics& A);
+void Bar(int n, characteristics& A);
+void OnTheShip(int n, characteristics& A);
+void OnTheGoldenHeart(int n, characteristics& A);
+void RestaurantAtTheEndOfTheUniverse(int n, characteristics& A);
+void ArrivalAtMigratea(int n, characteristics& A);
+void DialogueWithMarvin(int n, characteristics& A);
+void Slartibartfast(int n, characteristics& A);
---
+++ struct.h
@@ -0,0 +1,7 @@
+#pragma once
+
+static struct characteristics {
+	int Strength = 1;
+	int Dexterity = 2;
+	int Intelligence = 1;
+} Attributes;
